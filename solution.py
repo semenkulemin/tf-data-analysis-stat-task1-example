f8025bb2688a996edc06a97d2236c56ca46d9153
semenkulemin <55997982+semenkulemin@users.noreply.github.com>
2023-04-06 00:22:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 341395919 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    x = x / 169
+    return numpy.median(x) # Ваш ответ
